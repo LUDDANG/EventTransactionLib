1791ee62cf3b177d1cf8f073cef1de8e430c90c9
skywolf46 <skywolf461@gmail.com>
2025-01-03 12:53:07 +0900

    feat: bukkit 모듈 소스 코드 커밋

---
+++ modules/bukkit/src/main/java/live/luya/eventtransactionlib/EventTransactionPlugin.java
@@ -0,0 +1,12 @@
+package live.luya.eventtransactionlib;
+
+import live.luya.eventtransactionlib.internal.EventTransactionApiImpl;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventTransactionPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		EventTransactionApiProvider.setApi(new EventTransactionApiImpl());
+		EventTransactionApi.getApi().stackRegistrationOrder(RegistrationOrder.BUKKIT);
+	}
+}
